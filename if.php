eb41c6da7d81c9500c3c71df8a8ba449966a8fe9
torotw <torotw@gmail.com>
2020-03-25 14:10:03 +0800

    if2

--- if.php
+++ if.php
@@ -26,13 +26,33 @@ echo "<br>";
 
 
 //&&二者皆成立
-if ($year % 4 == 0 && ($year % 100 != 0 or $year % 400 == 0 ) && $year % 3200 != 0) {
+if ($year % 4 == 0 && ($year % 100 != 0 or $year % 400 == 0)) {
 
 
   echo "閏年";
-
-
-
 } else {
   echo "平年";
 }
+
+$level = "A";
+echo "等級:" . $level;
+echo "<br>";
+
+switch ($level) {
+  case "A":
+    echo "非常好，請努力保持";
+    break;
+  case "B":
+    echo "可圈可點，但還有進步的空間";
+    break;
+  case "C":
+    echo "一般水平，需要更多的努力";
+    break;
+  case "D":
+    echo "用心不足，請加強";
+    break;
+
+}
+
+
+?>
