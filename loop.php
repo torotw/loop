9a6d79be92fb49ad98c4694138ec463fdf92942b
torotw <torotw@gmail.com>
2020-03-25 16:11:31 +0800

    迴圈測試

--- loop.php
+++ loop.php
@@ -13,12 +13,31 @@ for($i=1;$i<=9;$i=$i+1){
     echo "<tr>";
     for($j=1;$j<=9;$j++){
 
-    echo "<td>"g.$j."x".$i."=".($i*$j)."</td>";
+    echo "<td>&nbsp".$j."x".$i."=".($i*$j)."&nbsp</td>";
     }
     echo "</tr>";
     echo "<br>";
     // echo "<hr>";
 }
+echo "</table>";
+
+
+echo "<hr>";
+//99乘法表形式2
+echo "<table border=1>";
+
+
+for($i=1;$i<=9;$i=$i+1){
+    echo "<tr>";
+    for($j=1;$j<=9;$j++){
+
+    echo "<td>&nbsp".($i*$j)."&nbsp</td>";
+    }
+    echo "</tr>";
+    echo "<br>";
+    // echo "<hr>";
+}
+
 echo "</table>";
 ?>
 
