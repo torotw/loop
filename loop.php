4787f240c3e98bb1310202a25464a6a0a39343a0
torotw <torotw@gmail.com>
2020-03-25 15:54:59 +0800

    999乘法表迴

--- loop.php
+++ loop.php
@@ -5,12 +5,20 @@
 for($i=0; $i<10;$i=$i+1){
 echo "hello" . $i . "<br>";
  
-
 }
+echo "<hr>";
+//99乘法表
+echo "<table border=1>";
+for($i=1;$i<=9;$i=$i+1){
+    echo "<tr>";
+    for($j=1;$j<=9;$j++){
 
-
-
-
-
-
+    echo "<td>"g.$j."x".$i."=".($i*$j)."</td>";
+    }
+    echo "</tr>";
+    echo "<br>";
+    // echo "<hr>";
+}
+echo "</table>";
 ?>
+
