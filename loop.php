b9bf68d8e5e3c80c7aac0cc22148fb0a48a0096a
torotw <torotw@gmail.com>
2020-03-26 08:23:37 +0800

    99乘法表第二種

--- loop.php
+++ loop.php
@@ -27,17 +27,66 @@ echo "<hr>";
 echo "<table border=1>";
 
 
-for($i=1;$i<=9;$i=$i+1){
+for($i=0;$i<=10;$i=$i+1){
     echo "<tr>";
+    if($i==0){
+        echo "<td>&nbsp"."</td>";  
+}else {
     for($j=1;$j<=9;$j++){
-
-    echo "<td>&nbsp".($i*$j)."&nbsp</td>";
-    }
-    echo "</tr>";
+   
+        echo "<td>&nbsp".($i*$j)."&nbsp</td>";
+}  
+for($j=1;$j<=9;$j++){
+   
+    echo "<td>&nbsp".($i*$j)."&nbsp</td>";   
+}
+echo "</tr>";
     echo "<br>";
     // echo "<hr>";
 }
+}  
 
 echo "</table>";
-?>
 
+
+
+echo "<hr>";
+
+//99乘法表形式2
+echo "<table border=1>";
+
+
+for($i=0;$i<10;$i=$i+1){
+    
+    if($i==0){
+  echo "<tr>";
+        echo "<td>&nbsp"."</td>";  
+    for($j=1;$j<=9;$j++){
+   echo "<td>&nbsp".$j."&nbsp</td>";
+    }
+}elseif($i>0){
+    echo "<td>".$i."</td>";
+    for($j=1;$j<=9;$j++){
+        echo "<td>&nbsp".($i*$j)."&nbsp</td>";
+         }  
+}  
+
+echo "</tr>";
+    echo "<br>";
+    // echo "<hr>";
+}
+  
+
+echo "</table>";
+
+echo "<hr>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+?>
